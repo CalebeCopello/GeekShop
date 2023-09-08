bf0a579f1efda9648dafe4a4496cce0cdc5f1bd0
CalebeCopello <114925816+CalebeCopello@users.noreply.github.com>
2023-09-08 18:01:29 -0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 import express from 'express' // AKA const express = require('express')
+import products from './data/products.js' // since it's my own js module I have to put .js
 
 //constants
 const PORT = 5000
@@ -6,6 +7,15 @@ const APP = express()
 
 APP.get('/', (req, res) => {
     res.send('API is running...')
+})
+
+APP.get('/api/products', (req,res) =>{
+    res.json(products)
+})
+
+APP.get('/api/products/:id', (req, res) => {
+    const product = products.find((p) => p._id === req.params.id)
+    res.json(product)
 })
 
 APP.listen(PORT, () => console.log(`Server running\nhttp://localhost:${PORT}`))
