2f13d188a688dc274bf6024a67dee0078b39fad0
CalebeCopello <114925816+CalebeCopello@users.noreply.github.com>
2023-09-07 21:07:05 -0300

    Rating ok

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -1,21 +1,25 @@
 import { Card } from "react-bootstrap"
 import { Link } from "react-router-dom"
+import Rating from "./Rating"
 
 const Product = ({p}) => {
     return (
         <>
-        <Card className='my-3 p-3 rounded bg-light shadow'>
+        <Card className='my-3 p-3 rounded bg-light shadow' style={{ width: '100%', height: '90%' }}>
             <Link to={`/product/${p._id}`}>
                 <Card.Img src={p.image} variant='top' className="rounded border border-dark bg-white" />
             </Link>
             <Card.Body>
                 <Link to={`/product/${p._id}`}>
-                    <Card.Title ad='div'>
+                    <Card.Title as='div'>
                         <strong>{p.name}</strong>
                     </Card.Title>
                 </Link>
-                <Card.Text as="h4">
-                    R$: {p.price}
+                <Card.Text as='div'>
+                    <Rating value={p.rating} text={`${p.numReviews} reviews`} />
+                </Card.Text>
+                <Card.Text as='h6'>
+                    {p.price.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL', minimumFractionDigits: 2 })}
                 </Card.Text>
             </Card.Body>
         </Card>
