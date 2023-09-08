ab2ffb6cfda40d1016167b61b911b4761ccde936
CalebeCopello <114925816+CalebeCopello@users.noreply.github.com>
2023-09-08 17:49:48 -0300

    Server set

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,11 @@
-console.log('Hello, World!')
+import express from 'express' // AKA const express = require('express')
+
+//constants
+const PORT = 5000
+const APP = express()
+
+APP.get('/', (req, res) => {
+    res.send('API is running...')
+})
+
+APP.listen(PORT, () => console.log(`Server running\nhttp://localhost:${PORT}`))
