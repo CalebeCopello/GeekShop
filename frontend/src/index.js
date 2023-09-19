f6d3246ec4573bce0d103d296a2c621604d1f1f4
CalebeCopello <114925816+CalebeCopello@users.noreply.github.com>
2023-09-19 19:49:46 -0300

    Private Route Done

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -14,6 +14,7 @@ import CartScreen from './screens/CartScreen'
 import LoginScreen from './screens/LoginScreen.jsx'
 import RegisterScreen from './screens/RegisterScreen.jsx'
 import ShippingScreen from './screens/ShippingScreen.jsx'
+import PrivateRoute from './components/PrivateRoute.jsx'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -23,7 +24,11 @@ const router = createBrowserRouter(
       <Route path='/cart' element={<CartScreen />} />
       <Route path='/login' element={<LoginScreen />} />
       <Route path='/register' element={<RegisterScreen />} />
-      <Route path='/shipping' element={<ShippingScreen />} />
+
+
+      <Route path='' element={<PrivateRoute />}>
+        <Route path='/shipping' element={<ShippingScreen />} />
+      </Route>
     </Route>
   )
 )
