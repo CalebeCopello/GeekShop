5895d9468c27059eac8a9b7ccacd4c59b887b486
CalebeCopello <114925816+CalebeCopello@users.noreply.github.com>
2023-09-19 14:46:25 -0300

    Register Done

--- frontend/src/screens/RegisterScreen.jsx
+++ frontend/src/screens/RegisterScreen.jsx
@@ -4,19 +4,21 @@ import { Form, Button, Row, Col } from 'react-bootstrap'
 import FormContainer from "../components/FormContainer"
 import { useDispatch, useSelector } from "react-redux"
 import Loader from "../components/Loader"
-import { useLoginMutation } from '../slices/userApiSlice'
+import { useRegisterMutation } from '../slices/userApiSlice'
 import { setCredentials } from "../slices/authSlice"
 import { toast } from "react-toastify"
 
 
-const LoginScreen = () => {
+const RegisterScreen = () => {
+    const [name, setName] = useState('')
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
+    const [confirmPassword, setConfirmPassword] = useState('')
 
     const dispatch = useDispatch()
     const navigate = useNavigate()
 
-    const [login, {isLoading}] = useLoginMutation()
+    const [register, {isLoading}] = useRegisterMutation()
 
     const { userInfo } = useSelector((state) => state.auth)
 
@@ -32,19 +34,28 @@ const LoginScreen = () => {
 
     const submitHandler = async (e) => { 
         e.preventDefault()
-        try {
-            const res = await login({email, password}).unwrap()
-            dispatch(setCredentials({...res, }))
-            navigate(redirect)
-        } catch (err) {
-            toast.error(err?.data?.message || err.error)
+        if (password !== confirmPassword) {
+            toast.error('Passwords são diferentes')
+            return
+        } else {
+            try {
+                const res = await register({name, email, password}).unwrap()
+                dispatch(setCredentials({...res, }))
+                navigate(redirect)
+            } catch (err) {
+                toast.error(err?.data?.message || err.error)
+            }
         }
     }
     return (
         <>
         <FormContainer>
-            <h1>Entrar</h1>
+            <h1>Registrar</h1>
             <Form onSubmit={submitHandler}>
+                <Form.Group controlId="name" className="my-3">
+                    <Form.Label>Nome</Form.Label>
+                    <Form.Control type='text' placeholder="Coloque seu Nome" value={name} onChange={(e) => setName(e.target.value)}></Form.Control>
+                </Form.Group>
                 <Form.Group controlId="email" className="my-3">
                     <Form.Label>Endereço de E-mail</Form.Label>
                     <Form.Control type='email' placeholder="Coloque seu E-mail" value={email} onChange={(e) => setEmail(e.target.value)}></Form.Control>
@@ -53,14 +64,18 @@ const LoginScreen = () => {
                     <Form.Label>Senha</Form.Label>
                     <Form.Control type='password' placeholder="Coloque sua Senha" value={password} onChange={(e) => setPassword(e.target.value)}></Form.Control>
                 </Form.Group>
+                <Form.Group controlId="confirmPassword" className="my-3">
+                    <Form.Label>Confirme sua Senha</Form.Label>
+                    <Form.Control type='password' placeholder="Confirme sua Senha" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)}></Form.Control>
+                </Form.Group>
                 <Button type="submit" variant="primary" className="mt-2" disabled={isLoading}>
-                    Entrar
+                    Registrar
                 </Button>
                 {isLoading && <Loader />}
             </Form>
             <Row className="py-3">
                 <Col>
-                    Cliente Novo? <Link to={redirect ? `/register?redirect=${redirect}` : '/register'}>Registrar-se</Link>
+                    Já tem uma Conta? <Link to={redirect ? `/login?redirect=${redirect}` : '/login'}>Entrar</Link>
                 </Col>
             </Row>
         </FormContainer>
@@ -68,4 +83,4 @@ const LoginScreen = () => {
     )
 }
 
-export default LoginScreen
+export default RegisterScreen
