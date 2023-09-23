0939ac69f9c08150c4799f3233cccb3b4facc756
CalebeCopello <114925816+CalebeCopello@users.noreply.github.com>
2023-09-23 16:52:54 -0300

    fix paymentResult
    fix getting results from PayPal API

--- frontend/src/screens/OrderScreen.jsx
+++ frontend/src/screens/OrderScreen.jsx
@@ -15,21 +15,23 @@ const OrderScreen = () => {
 
     const [payOrder, {isLoading: loadingPay}] = usePayOrderMutation()
 
-    const [{isPending}, paypayDispatch] = usePayPalScriptReducer()
+    const [{isPending}, paypalDispatch] = usePayPalScriptReducer()
 
     const {data: paypal, isLoading: loadingPayPal, error: errorPayPal} = useGetPayPalClientIdQuery()
+    
+    const {userInfo} = useSelector((state) => state.auth)
 
     useEffect(() =>{
         if (!errorPayPal && !loadingPayPal && paypal.clientId) {
             const loadPayPalScript = async () => {
-                paypayDispatch({
+                paypalDispatch({
                     type: 'resetOptions',
                     value: {
                         'client-id': paypal.clientId,
                         currency: 'BRL',
                     }
                 })
-                paypayDispatch({ type: 'setLoadingStatus', value: 'pending'})
+                paypalDispatch({ type: 'setLoadingStatus', value: 'pending'})
             }
             if (order && !order.isPaid) {
                 if (!window.paypal) {
@@ -37,9 +39,41 @@ const OrderScreen = () => {
                 }
             }
         }
-    },[order, paypal, paypayDispatch, loadingPayPal, errorPayPal])
+    },[order, paypal, paypalDispatch, loadingPayPal, errorPayPal])
 
-    const {userInfo} = useSelector((state) => state.auth)
+
+    function onApprove(data, actions) {
+        return actions.order.capture().then(async function (details) {
+            try {
+                await payOrder({orderId, details})
+                refetch()
+                toast.success('Pago com Sucesso!')
+            } catch (err) {
+                toast.error(err?.data?.message || err.message)
+            }
+        })
+    }
+    async function onApproveTest() {
+        await payOrder({orderId, detais: {payer: {}}})
+        refetch()
+        toast.success('Pago com Sucesso!')
+    }
+    function onError(err) {
+        toast.error(err.message)
+    }
+    function createOrder(data, actions) {
+        return actions.order.create({
+            purchase_units: [
+                {
+                    amount: {
+                        value: order.totalPrice.toFixed(2),
+                    }
+                }
+            ]
+        }).then((orderId) => {
+            return orderId
+        })
+    }
 
     return isLoading ? <Loader /> : error ? <Message variant='danger' /> : (
         <>
@@ -116,7 +150,19 @@ const OrderScreen = () => {
                                 <Col>{order.totalPrice.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL', minimumFractionDigits: 2 })}</Col>
                             </Row>
                         </ListGroup.Item>
-                        {/*TODO: PAY ORDER PLACEHOLDER */}
+                        {!order.isPaid && (
+                            <ListGroup.Item>
+                                {loadingPay && <Loader />}
+                                {isPending ? <Loader /> : (
+                                    <div>
+                                        <Button onClick={onApproveTest} style={{marginBottom: '50px'}}>Teste Pay Order</Button>
+                                        <div>
+                                            <PayPalButtons createOrder={createOrder} onApprove={onApprove} onError={onError}></PayPalButtons>
+                                        </div>
+                                    </div>
+                                )}
+                            </ListGroup.Item>
+                        )}
                         {/*TODO: MARK AS DELIVERED PLACEHOLDER */}
                     </ListGroup>
                 </Card>
