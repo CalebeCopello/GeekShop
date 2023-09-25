f4836bd476ec0885f512ac8041c3b10db30e980d
CalebeCopello <114925816+CalebeCopello@users.noreply.github.com>
2023-09-25 19:38:40 -0300

    Edit Product Done

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -28,18 +28,43 @@ const getProductsById = asyncHandler(async (req,res) =>{
 // @acess Private/Admin
 const createProduct = asyncHandler(async (req,res) =>{
     const product = new Product({
-        name: 'Nome de modelo',
+        name: 'Nome modelo',
         price: 0,
         user: req.user._id,
         image: '/images/Sample.png',
-        brande: 'Marca de modelo',
-        category: 'Categoria de modelo',
+        brand: 'Marca modelo',
+        category: 'Categoria modelo',
         countInStock: 0,
         numReviews: 0,
-        description: 'Descrição de modelo',
+        description: 'Descrição modelo',
     })
-    const createProduct = await product.save()
-    res.status(201).json(createProduct)
+    const createdProduct = await product.save()
+    res.status(201).json(createdProduct)
 })
 
-export {getProducts, getProductsById, createProduct}
+// @desc Update a product
+// @route PUT /api/products/:id
+// @acess Private/Admin
+const updateProduct = asyncHandler(async (req,res) =>{
+    const {name, price, description, image, brand, category, countInStock} = req.body
+
+    const product = await Product.findById(req.params.id)
+
+    if(product) {
+        product.name = name
+        product.price = price
+        product.description = description
+        product.image = image
+        product.brand = brand
+        product.category = category
+        product.countInStock = countInStock
+
+        const updatedProduct = await product.save()
+        res.json(updatedProduct)
+    } else {
+        res.status(404)
+        throw new Error('Recurso não encontrado')
+    }
+})
+
+export {getProducts, getProductsById, createProduct, updateProduct}
