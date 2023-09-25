d8578f34091cda1202ec7a0bdd1dec9875d5ed7c
CalebeCopello <114925816+CalebeCopello@users.noreply.github.com>
2023-09-25 16:27:44 -0300

    OrderList Done

--- frontend/src/screens/admin/OrderListScreen.jsx
+++ frontend/src/screens/admin/OrderListScreen.jsx
@@ -1,8 +1,55 @@
-
+import {LinkContainer} from 'react-router-bootstrap'
+import {Table, Button} from 'react-bootstrap'
+import {FaTimes} from 'react-icons/fa'
+import Message from '../../components/Message'
+import Loader from '../../components/Loader'
+import {useGetOrdersQuery} from '../../slices/ordersApiSlice'
 
 const OrderListScreen = () => {
+    const {data: orders, isLoading, error} = useGetOrdersQuery()
+    
     return(
         <>
+        <h1>Pedidos</h1>
+        {isLoading ? (<Loader />) : error ? (<Message variant='danger'>{error}</Message>) : (
+            <Table striped hover responsive className='table-sm'>
+                <thead>
+                    <tr>
+                    <th>ID</th>
+                    <th>Usuário</th>
+                    <th>Data</th>
+                    <th>Total</th>
+                    <th>Pago</th>
+                    <th>Entregue</th>
+                    <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {orders.map((order) => (
+                        <tr key={order._id}>
+                            <td>{order._id}</td>
+                            <td>{order.user && order.user.name}</td>
+                            <td>{order.createdAt.substring(0,10)}</td>
+                            <td>{order.totalPrice.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})}</td>
+                            <td>
+                            {order.isPaid ? (order.paidAt.substring(0,10)) : (<FaTimes style={{ color: 'red' }} />)}
+                            </td>
+                            <td>{order.totalPrice.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})}</td>
+                            <td>
+                            {order.isDelivered ? (order.Delivered.substring(0,10)) : (<FaTimes style={{ color: 'red' }} />)}
+                            </td>
+                            <td>
+                                <LinkContainer to={`/order/${order._id}`}>
+                                    <Button variant='light' className='btn-sm'>
+                                        Detalhes
+                                    </Button>
+                                </LinkContainer>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </Table>
+        )}
         </>
 
     )
